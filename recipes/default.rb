2c3cfe4f85ac9a9d661937bc265a5662a3e99522
Antek Baranski <antek.baranski@disney.com>
2014-06-17 17:42:36 -0700

    adding sendmail.mc

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,17 +2,19 @@ package 'sendmail' do
 	action :install
 end
 
-
 service 'sendmail' do
 	action [ :enable, :start ]
 end
 
-
 template "/etc/mail/sendmail.cf" do
  	source    "sendmail.cf.erb"
- 	notifies  :restart,"service[sendmail]"
+ 	notifies  :restart,"service[sendmail]", :delayed
 end
 
+template "/etc/mail/sendmail.mc" do
+  source    "sendmail.mc.erb"
+  notifies  :restart,"service[sendmail]", :delayed
+end
 
 # template "/etc/mail/relay-domains" do
 # 	source "relay-domains"
